55348eb5a701f9abe319eabfc89f29c1ad21cbcf
Graf-Carello <felix.graf@edu.fh-joanneum.at>
2015-11-26 20:50:45 +0100

    test impl

--- homework/avatarix/src/test/AvatarixTest.scala
+++ homework/avatarix/src/test/AvatarixTest.scala
@@ -1,6 +1,7 @@
 package fhj.swengb.avatarix
 
 
+import fhj.swengb.{Students, GitHub}
 import org.junit.Assert._
 import org.junit.Test
 
@@ -8,14 +9,15 @@ import org.junit.Test
   * Created by graf, hasenbichler, koerner on 14.11.15.
   */
 
-class AvatarixTest {
+class AvatarixTest{
 
 
-  @Test def correctAvatarUrl(): Unit = {
-    //assertEquals(irgendwas == anzahlDerStudenten)
+  @Test def isValidUrl(): Unit = {
+    assertTrue(Students.jblazevic.firstName == "Josip")
   }
 
   @Test def correctHtmlUrl(): Unit = {
+    assertEquals(1,1)
     //assertTrue(Student existiert nur einmal)
   }
 
